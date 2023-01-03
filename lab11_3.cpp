05d396a574be4b64faee6c3750d46b06bbe0f14b
lan <Kanonlas_r@cmu.ac.th>
2023-01-03 16:13:49 +0700

    edited

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string>
+#include<cmath>
+#include<cstdlib>
+#include <iomanip>
+using namespace std;
+int main(){
+
+ int count = 0;
+ float sum = 0;
+ float sum1 = 0;
+ string textline;
+ ifstream source("score.txt");
+ while(getline(source, textline)){
+    count++;
+    sum += atof(textline.c_str());
+    sum1 += pow(atof(textline.c_str()), 2);
+ }
+
+
+
+cout << "Number of data = " << count << endl;
+cout << setprecision(3);
+cout << "Mean = " << sum/count << endl;
+cout << "Standard deviation = " << sqrt(((sum1/count) - pow((sum/count), 2)));
+
+return 0;
+}
