146eb3a80c5e931296cc7917412e1ca1c931012b
lan <Kanonlas_r@cmu.ac.th>
2023-01-03 15:56:51 +0700

    edited

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -8,7 +8,13 @@ int main (){
 	ofstream dest;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
-	
+	dest << "-------------------- BOOM ---------------------" << endl;
+	string textline;
+	while(getline(source, textline)){
+	dest << textline << "\n";
+  }
+  
+	dest << "-------------------- HA!! ---------------------" ;
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
